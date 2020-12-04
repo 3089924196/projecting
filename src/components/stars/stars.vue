5d68906b080aa88045009cafe809ae3810bad66e
heqinglon2 <3089924196@qq.com>
2020-12-04 14:23:05 +0800

    提交2

--- src/components/stars/stars.vue
+++ src/components/stars/stars.vue
@@ -24,7 +24,7 @@ export default {
       return [OFF,OFF,OFF,OFF,OFF]
       let arr=[];
       let score=Math.floor(this.score*2)/2;
-      let ONSize=Math.floor(score*2);
+      let ONSize=Math.floor(score);
       for(let i=0;i<ONSize.length;i++){
       arr.push(ON)
       }
@@ -39,19 +39,47 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import "../../common/stylus/mixin.styl"
     .stars
         display inline-flex
         & > .star
-          background pink
             background-size 100%
             background-repeat no-repeat
             &.star-48
                 width 20px
                 height 20px
+                margin-right 20px
+                &:last-child
+                    margin-right 0px
+                &.on
+                    bg-image(star48_on)
+                &.half
+                    bg-image(star48_half)
+                &.off
+                    bg-image(star48_off)
+                
             &.star-36
                 width 15px
                 height 15px
+                margin-right 15px
+                &:last-child
+                    margin-right 0px
+                &.on
+                    bg-image(star36_on)
+                &.half
+                    bg-image(star36_half)
+                &.off
+                    bg-image(star36_off)
             &.star-24
                 width 10px
                 height 10px
+                margin-right 10px
+                &:last-child
+                    margin-right 0px
+                &.on
+                    bg-image(star24_on)
+                &.half
+                    bg-image(star24_half)
+                &.off
+                    bg-image(star24_off)
 </style>
--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -1,13 +1,91 @@
 <template>
- <div>goods</div>
+<div class="goodsWrap">
+<div class="goods">
+<div class="left" ref="left">
+<ul class="typeList" ref="typeList">
+  <li class="typeItem" :class="{active:currentIndex===index}"
+  v-for="(good,index) in goods" :key="index" @click=leftToRight(index)>
+  <seller-icon class="icon" v-show="good.type>=0" :type="good.type" size="3" ></seller-icon>
+  <span class="name">{{good.name}}</span>
+  </li>
+</ul>
+</div>
+<div class="right">
+  <ul class="classList" ref="classList">
+    <li class="classItem" v-for="(good,index) in goods" :key="index">
+      <h2 class="title">{{good.name}}</h2>
+      <ul class="foodList">
+        <li class="foodItem" v-for="(good,index) in goods" :key="index">
+          <seller-food :food="food"></seller-food>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</div>
+   </div>
+   <div class="cart"></div>
+ </div>
 </template>
-
 <script>
+import {GETSELLER,GETGOODS,GETRATINGS} from "store/mutation_types.js";
+import BetterScroll from 'better-scroll';
+import {mapState,mapActions} from "vuex"
+import food from 'components/food/food';
 export default {
-  name: "goods"
+  name: "goods",
+  data(){
+    return{
+
+    }
+  },
+  async mounted() {
+    await this[GETSGOODS]();
+  },
+  computed:{
+    ...mapState(["goods","iconTypes"]),
+  },
+methods: {
+  ...mapActions([GETGOODS]),
+  
+},
+components:{
+  "seller-food":food
+}
 }
 </script>
 
 <style scoped lang="stylus">
+@import "../../common/stylus/mixin.styl"
+.goodsWrap
+    display flex
+    flex-direction column
+    .goods
+        flex 1
+        display flex
+        overflow hiddeen
+        .left
+            width 80px
+            .typeList
+                .typeItem
+                    display flex
+                    justify-content center
+                    align-items center
+                    box-sizing border-box
+                    width 80px
+                    height 54px
+                    padding 0 12px
+                    background #f3f5f7
+                    font-size 12px
+                    font-weight 200
+                    line-height 174px
+                    color rgba(240,20,20)
+                    &.active
+                        background pink
+                    &:after
+                        width 56px
+                    
+
+
+
 
 </style>
